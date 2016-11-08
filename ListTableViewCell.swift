44ea6ae51086bd60b90565c453fd53a65464fdb7
KimNeutral <rlaeodyd01@dgsw.hs.kr>
2016-11-08 19:26:09 +0900

    re upload

---
+++ ListTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  listTableViewCell.swift
+//  Layout
+//
+//  Created by user on 2016. 10. 21..
+//  Copyright © 2016년 DGSW. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var listTitle: UILabel!
+    @IBOutlet weak var listDeadLine: UILabel!
+    
+    @IBOutlet weak var listImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MainViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MainViewController.swift
+//  Layout
+//
+//  Created by dshs_student on 2016. 10. 6..
+//  Copyright © 2016년 DGSW. All rights reserved.
+//
+
+import UIKit
+
+var listTitles = ["나르샤캠프 iOS반 숙제", "나르샤캠프 iOS반 어플 마무리짓기"]
+var listDeadLines = ["2016.10.22 12:00까지", "2016.12.31 09:00까지"]
+var listImages = []
+
+class MainViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource{
+    
+    var searchController:UISearchController!
+    
+    let noDataLabel = UILabel()
+    
+    @IBAction func Search(sender: AnyObject) {
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.searchBar.delegate = self
+        presentViewController(searchController, animated: true, completion: nil)
+    }
+    @IBOutlet weak var mainTableView: UITableView!
+    
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listTitles.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell1",forIndexPath: indexPath) as! ListTableViewCell
+        cell.listTitle.text = listTitles[indexPath.row]
+        cell.listDeadLine.text = listDeadLines[indexPath.row]
+        cell.listImageView = nil
+        return cell
+    }
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool
+    {
+        return true
+    }
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            listTitles.removeAtIndex(indexPath.row)
+            listDeadLines.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+            
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTableView.dataSource = self
+        mainTableView.delegate = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
