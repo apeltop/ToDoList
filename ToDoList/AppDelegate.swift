d2d61ff408f02acd8012c6f1e558a3edd383519e
dshs_student <std01@dshs-students-Mac-mini.local>
2016-12-03 18:03:33 +0900

    정한나, 이소진 백그라운드 알림 기능 구현

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -10,38 +10,60 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        application.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
         return true
     }
-
+    
+    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+        let date = NSDate()
+        let formatter = NSDateFormatter()
+        
+        let time = formatter.stringFromDate(date)
+        
+        print(time)
+        //print("Background fetch executed \"Debug ->Simulate Background Fetch\"!!!")
+        //completionHandler(UIBackgroundFetchResult.NewData)
+    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
     }
-
+    
+    
     func applicationDidEnterBackground(application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let app = UIApplication.sharedApplication()
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Alert], categories: nil)
+        app.registerUserNotificationSettings(notificationSettings)
+        let alertTime = NSDate().dateByAddingTimeInterval(1)
+        let notifyAlarm = UILocalNotification()
+        
+        notifyAlarm.fireDate = alertTime
+        notifyAlarm.timeZone = NSTimeZone.defaultTimeZone()
+        notifyAlarm.alertBody = "important metting in 30 minutes"
+        app.scheduleLocalNotification(notifyAlarm)
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
+    
     // MARK: - Split view
-
 }
-
