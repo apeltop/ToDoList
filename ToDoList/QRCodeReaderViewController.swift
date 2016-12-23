f0c93f162f3b7e8a23afd7de45fb1c832cd7e243
KimNeutral <rlaeodyd01@dgsw.hs.kr>
2016-12-23 09:12:38 +0900

    설정 아이콘을 qr코드 아이콘으로 변경 qr코드 리더 코드 작업중

---
+++ ToDoList/QRCodeReaderViewController.swift
@@ -0,0 +1,57 @@
+//
+//  QRCodeReaderViewController.swift
+//  ToDoList
+//
+//  Created by dshs_student on 2016. 12. 23..
+//  Copyright © 2016년 DGSW_TEACHER. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class QRCodeReaderViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var messageLabel:UILabel!
+    
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            
+            captureSession = AVCaptureSession()
+            
+            captureSession?.addInput(input)
+            
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+        } catch{
+            print(error)
+            return
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
+        
+        view.bringSubviewToFront(messageLabel)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
